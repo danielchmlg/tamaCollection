aed112947518edd8cc28cff5f8cdacc9a938be26
danielCuevasDeHaro <danielchmlg@gmail.com>
2025-10-29 14:44:55 +0100

    Layout activity_add_tama.xml corregido y creada lógica para almacenar tamagotchi con todos los datos. Se añaden lineas en el fichero strings.xml

--- app/src/main/java/com/danielcuevasdeharo/tamacollection/addtama/AddTamaActivity.kt
+++ app/src/main/java/com/danielcuevasdeharo/tamacollection/addtama/AddTamaActivity.kt
@@ -1,5 +1,6 @@
 package com.danielcuevasdeharo.tamacollection.addtama
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -8,10 +9,12 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.danielcuevasdeharo.tamacollection.MenuActivity
 import com.danielcuevasdeharo.tamacollection.R
+import com.danielcuevasdeharo.tamacollection.sqlitedb.Adquisicion
+import com.danielcuevasdeharo.tamacollection.sqlitedb.Comercio
 import com.danielcuevasdeharo.tamacollection.sqlitedb.TamaSQLite
 import com.danielcuevasdeharo.tamacollection.sqlitedb.Tamagotchi
+import java.util.Calendar
 
 private lateinit var dbAdd: TamaSQLite
 
@@ -30,21 +33,40 @@ class AddTamaActivity : AppCompatActivity() {
         val etGenTama = findViewById<EditText>(R.id.etGenTama)
         val etYearTama = findViewById<EditText>(R.id.etYearTama)
         val etPriceTama = findViewById<EditText>(R.id.etPriceTama)
+        val etComName = findViewById<EditText>(R.id.etComName)
+        val etComUbi = findViewById<EditText>(R.id.etComUbi)
+        val etAdqDate = findViewById<EditText>(R.id.etAdqDate)
+
+        etAdqDate.setOnClickListener {
+            viewDatePicker(etAdqDate)
+        }
 
         btnSaveTama.setOnClickListener {
-                val idTama = etIdTama.text.toString().toInt()
-                val nameTama = etNameTama.text.toString()
-                val genTama = etGenTama.text.toString()
-                val yearTama = etYearTama.text.toString().toInt()
-                val priceTama = etPriceTama.text.toString().toDouble()
+            val idTama = etIdTama.text.toString().toInt()
+            val nameTama = etNameTama.text.toString()
+            val genTama = etGenTama.text.toString()
+            val yearTama = etYearTama.text.toString().toInt()
+            val priceTama = etPriceTama.text.toString().toDouble()
+            val comName = etComName.text.toString()
+            val comUbi = etComUbi.text.toString()
+            val adqDate = etAdqDate.text.toString()
 
-                val tamaAdd = Tamagotchi(idTama, nameTama, genTama, yearTama)
+            val tamaAdd = Tamagotchi(idTama, nameTama, genTama, yearTama)
+            dbAdd.insert(tamaAdd)
+            val comAdd = Comercio(comName = comName, ubication = comUbi)
+            val idcom = dbAdd.insertComercio(comAdd)
 
-                dbAdd.insert(tamaAdd)
+            val adqAdd = Adquisicion(
+                tamaId = idTama,
+                comId = idcom.toInt(),
+                date = adqDate,
+                price = priceTama
+            )
+            dbAdd.insertAdquisicion(adqAdd)
 
-                Toast.makeText(this, "Se ha registrado el id $idTama", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MenuActivity::class.java)
-                startActivity(intent)
+
+            Toast.makeText(this, "Se ha registrado el id $idTama", Toast.LENGTH_SHORT).show()
+            finish()
 
         }
         btnBackAdd.setOnClickListener(object : View.OnClickListener {
@@ -55,9 +77,34 @@ class AddTamaActivity : AppCompatActivity() {
 
 
     }
-    private fun initComponent(){
+
+    private fun initComponent() {
         dbAdd = TamaSQLite(this)
         btnSaveTama = findViewById(R.id.btnSaveTama)
         btnBackAdd = findViewById(R.id.btnBackAdd)
+    }
+
+    private fun viewDatePicker(etAdqDate: EditText) {
+        val calendar = Calendar.getInstance()
+
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this, { _, selectedYear, selectedMonth, selectedDay ->
+                // El mes aparece del 0 al 11 para corregirlo le sumamos 1
+                val correctedMonth = selectedMonth + 1
+                //Formateamos la fecha a YYYY-MM-DD para poder guardar como TEXT en la BBDD
+                val bbddDate =
+                    String.format("%d-%02d-%02d", selectedYear, selectedMonth, selectedDay)
+
+                etAdqDate.setText(bbddDate)
+            },
+            year, month, day
+        )
+        datePickerDialog.show()
+
+
     }
 }
