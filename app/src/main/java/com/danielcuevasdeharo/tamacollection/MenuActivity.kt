68eb74d839b60d1b37b4c522616a86be89c6c07c
danielCuevasDeHaro <danielchmlg@gmail.com>
2025-11-06 16:56:23 +0100

    Pulido MenuActivity.kt se añade instancia a FirebaseAuth, se simplifican los setOnClickListener a través de la función navigate() a la cuál se le añade una condicional para establecer "banderas" si es necesario cerrar las activities anteriores (como ocurre en el caso de pulsar el botón de cerrar sesión)

--- app/src/main/java/com/danielcuevasdeharo/tamacollection/MenuActivity.kt
+++ app/src/main/java/com/danielcuevasdeharo/tamacollection/MenuActivity.kt
@@ -9,6 +9,7 @@ import com.danielcuevasdeharo.tamacollection.addtama.AddTamaActivity
 import com.danielcuevasdeharo.tamacollection.autentication.AuthActivity
 import com.danielcuevasdeharo.tamacollection.findtama.FindTamaActivity
 import com.danielcuevasdeharo.tamacollection.recycleview.RecycleViewActivity
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -23,42 +24,58 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        //inicializamos auth antes de usar
+        auth = FirebaseAuth.getInstance()
         initComponent()
         initListeners()
 
     }
 
-    private fun initComponent(){
-        btnClose= findViewById(R.id.btnClose)
-        btnNewTama= findViewById(R.id.btnNewTama)
-        btnFindTama= findViewById(R.id.btnFindTama)
-        btnColTama= findViewById(R.id.btnColTama)
+    private fun initComponent() {
+        btnClose = findViewById(R.id.btnClose)
+        btnNewTama = findViewById(R.id.btnNewTama)
+        btnFindTama = findViewById(R.id.btnFindTama)
+        btnColTama = findViewById(R.id.btnColTama)
     }
-    private fun initListeners(){
+
+    /*Cómo usaremos varios setOnClickListener, vamos a crear una función para simplificarlo
+   *Esta función recibirá por parámetros una clase java y un boolean inicializado en false
+   * Este boolean va a permitir establecer "banderas" para determinar si es necesario que las activities
+   * anteriores queden accesibles o no ya que en el caso de cerrar sesión nos interesa que no queden
+   * accesibles para que no puedan volver atrás y accedan a pantallas privadas de una sesión.
+   */
+    private fun navigate(to: Class<*>, clearTask: Boolean = false) {
+        Intent(this, to).apply {
+            //si clear task es true, entonces usaremos las banderas para dejar inaccesible las activities anteriores
+            if (clearTask) {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(this)
+        }
+
+    }
+
+    private fun initListeners() {
         btnClose.setOnClickListener {
+            //Para evitar doble click sobre el botón
+            it.isEnabled = false
+            //mensaje al cerrar sesión
             Toast.makeText(
                 this,
                 "Sesión cerrada, ¡hasta pronto!",
-                Toast.LENGTH_SHORT).show()
+                Toast.LENGTH_SHORT
+            ).show()
+            //Cierre de sesión
             auth.signOut()
+            //Navegamos a la pantalla de inicio de sesión
+            navigate(AuthActivity::class.java, true)
 
-            val intent = Intent (this, AuthActivity::class.java)
-            startActivity(intent)
-
         }
 
-        btnNewTama.setOnClickListener {
-            val intent= Intent(this, AddTamaActivity::class.java)
-            startActivity(intent)
-        }
-        btnFindTama.setOnClickListener {
-            val intent= Intent(this, FindTamaActivity::class.java)
-            startActivity(intent)
-        }
-        btnColTama.setOnClickListener {
-            val intent = Intent(this, RecycleViewActivity::class.java)
-            startActivity(intent)
-        }
+        btnNewTama.setOnClickListener { navigate(AddTamaActivity::class.java, false) }
+        btnFindTama.setOnClickListener { navigate(FindTamaActivity::class.java) }
+        btnColTama.setOnClickListener { navigate(RecycleViewActivity::class.java) }
     }
+
 
 }
